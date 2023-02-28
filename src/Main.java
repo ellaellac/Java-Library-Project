882cab8679298281d7963876d34afb4264d6c841
ellaellac <ellachoywy@gmail.com>
2023-02-28 19:43:12 +0000

    get data from csv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Library!");
+        String path = "C:/Users/cwyel/nology2/java-library/books_data.csv";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line="";
+
+            while((line = br.readLine())!= null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException error){
+            error.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
