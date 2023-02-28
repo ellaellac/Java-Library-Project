45b75380a4846cab283c283fc05fa46171a82a5e
ellaellac <ellachoywy@gmail.com>
2023-02-28 21:27:42 +0000

    store list of books

---
+++ src/Book.java
@@ -0,0 +1,86 @@
+public class Book {
+    private int number;
+    private String title;
+    private String Author;
+    private String Genre;
+    private String SubGenre;
+    private String Publisher;
+
+    //Constructor
+
+    public Book(int number, String title, String author, String genre, String subGenre, String publisher) {
+        this.number = number;
+        this.title = title;
+        Author = author;
+        Genre = genre;
+        SubGenre = subGenre;
+        Publisher = publisher;
+    }
+
+
+    //Getter and Setter
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public String getSubGenre() {
+        return SubGenre;
+    }
+
+    public void setSubGenre(String subGenre) {
+        SubGenre = subGenre;
+    }
+
+    public String getPublisher() {
+        return Publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        Publisher = publisher;
+    }
+
+    //toString
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "number='" + number + '\'' +
+                ", title='" + title + '\'' +
+                ", Author='" + Author + '\'' +
+                ", Genre='" + Genre + '\'' +
+                ", SubGenre='" + SubGenre + '\'' +
+                ", Publisher='" + Publisher + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         String path = "C:/Users/cwyel/nology2/java-library/books_data.csv";
 
+        ArrayList <Book> books = new ArrayList<>();
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
-            String line;
+            String line ;
+            br.readLine(); //Skip the first line as it reads the next line of text from the file and returns it as a string
 
             while((line = br.readLine())!= null){
-                String [] columns = line.split(",");
-                System.out.println(columns[1]);
+                String[] columns = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+                Book book = new Book(Integer.parseInt(columns[0]),columns[1],columns[2],columns[3],columns[4],columns[5]);
+                books.add(book);
             }
-        } catch (FileNotFoundException error){
-            error.printStackTrace();
+//            System.out.println(books);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
