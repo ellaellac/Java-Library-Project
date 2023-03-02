b3a11ea3f7335d9c007ebebb79e2aa066fca44bb
ellaellac <ellachoywy@gmail.com>
2023-03-02 01:00:32 +0000

    user class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
---
+++ src/User.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private static int id = 0;
+    private String name;
+    private List<Book> borrowedBooks = new ArrayList<>();
+
+    public User(String name, List<Book> borrowedBooks) {
+        this.name = name;
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        User.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", borrowedBooks=" + borrowedBooks +
+                '}';
+    }
+
+}
